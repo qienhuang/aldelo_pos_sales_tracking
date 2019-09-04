543f5d65b56fa27b584b1c2912c26d52a49e13cc
QIEN HUANG <qien.huang.ny@gmail.com>
2019-09-03 22:42:41 -0400

    Main go file

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	_ "log"
+	_ "time"
+
+	"sales_monitor/access"
+	_ "sales_monitor/config"
+	"sales_monitor/helper"
+	"sales_monitor/mysql"
+	"sales_monitor/scheduler"
+	"sales_monitor/webserver"
+
+	_ "github.com/alexbrainman/odbc"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func init() {
+
+}
+
+func main() {
+	fmt.Println("Sales tracker is starting ~~~~~~~~~~~~~~~~~~~~~~")
+
+	// catchs ctl+c SIGINT/SIGTERM signals, cleanup connections before exit
+	_, done := helper.ExitHandler()
+
+	access.InitOdbc()
+	mysql.InitMySql()
+
+	scheduler.Start()
+	webserver.Start()
+
+	// append cleanup functions to ExitHandler
+	helper.OnClosing(access.CloseOdbc)
+	helper.OnClosing(mysql.CloseMySql)
+	helper.OnClosing(webserver.Shutdown)
+
+	// blocks and wait for the ExitHandler
+	<-done
+
+}
